7d8b0085a0414c4a5c0b680e1082909b31d55448
zhaoyang <zhaoyang@lyky.cn>
2023-07-25 16:56:19 +0800

    update

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen'; // 控制启动页的包
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  // const [root, setRoot] = useState();
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync(); // 保证项目启动后，系统启动页一直显示
+  }, []);
+
+  setTimeout(() => {
+    setAppIsReady(true)
+  }, 5000)
+
+  if (!appIsReady) {
+    return null;
+  } else {
+    SplashScreen.hideAsync(); // 系统启动页消失（需要确保需要展示的界面已经准备好）
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
