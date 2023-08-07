316c0acb4c5a3dabebb52ed8ed3d0cbe6b74c95d
zhaoyang <zhaoyang@lyky.cn>
2023-08-07 15:42:28 +0800

    update

--- src/components/home-tab/growth/index.js
+++ src/components/home-tab/growth/index.js
@@ -1,10 +1,54 @@
 import React from 'react';
-import { Text } from '@rneui/themed';
+import {
+    StyleSheet
+} from 'react-native';
+import { Text, Card } from '@rneui/themed';
 
 export default () => {
     return (
         <>
-            <Text h5>growth</Text>
+            <Card>
+                <Text>2023-08-07 4M 20D</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>体重：12kg</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>
+                    身高：50cm
+                </Text>
+            </Card>
+            <Card>
+                <Text>2023-08-07 4M 20D</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>体重：12kg</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>
+                    身高：50cm
+                </Text>
+            </Card>
+            <Card>
+                <Text>2023-08-07 4M 20D</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>体重：12kg</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>
+                    身高：50cm
+                </Text>
+            </Card>
+            <Card>
+                <Text>2023-08-07 4M 20D</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>体重：12kg</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>
+                    身高：50cm
+                </Text>
+            </Card>
+            <Card>
+                <Text>2023-08-07 4M 20D</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>体重：12kg</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>
+                    身高：50cm
+                </Text>
+            </Card>
+            <Card>
+                <Text>2023-08-07 4M 20D</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>体重：12kg</Text>
+                <Text style={StyleSheet.create({ fontSize: 16, fontWeight: 'bold' })}>
+                    身高：50cm
+                </Text>
+            </Card>
         </>
     );
 };
