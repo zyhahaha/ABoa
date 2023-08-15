5d1d8f8794d49ce173d24086e3f8ff9a76b0f14d
zhaoyang <zhaoyang@lyky.cn>
2023-08-15 15:08:33 +0800

    update

--- src/components/home-tab/events/index.js
+++ src/components/home-tab/events/index.js
@@ -12,7 +12,8 @@ export default () => {
             <SafeAreaView>
                 <ScrollView>
                     <View style={{
-                        height: 500,
+                        // height: 'auto',
+                        marginBottom: 50,
                         paddingTop: 10,
                         paddingLeft: 10,
                         backgroundColor: '#F5F5F5'
@@ -23,7 +24,9 @@ export default () => {
                             borderLeftWidth: 2,
                             borderLeftColor: '#E4E4E4'
                         }}>
-                            <View>
+                            <View style={{
+                                marginBottom: 15
+                            }}>
                                 <View style={{
                                     marginLeft: -6,
                                     height: 10,
@@ -57,6 +60,84 @@ export default () => {
                                         fontSize: 12,
                                         color: '#8F8F8F'
                                     }}>6月17日 15.00</Text>
+                                </View>
+                            </View>
+
+                            <View style={{
+                                marginBottom: 15
+                            }}>
+                                <View style={{
+                                    marginLeft: -6,
+                                    height: 10,
+                                    width: 10,
+                                    borderRadius: 10,
+                                    borderStyle: 'solid',
+                                    borderColor: '#F3C026',
+                                    borderWidth: 2,
+                                    backgroundColor: '#F5F5F5'
+                                }}></View>
+                                <View style={{
+                                    marginTop: -15,
+                                    marginLeft: 10,
+                                    marginBottom: 10
+                                }}>
+                                    <Text>4月29日</Text>
+                                </View>
+                                <View style={{
+                                    marginLeft: 10,
+                                    padding: 10,
+                                    backgroundColor: '#fff'
+                                }}>
+                                    <Text style={{
+                                        fontSize: 14
+                                    }}>身高  54.5 cm</Text>
+                                    <Text style={{
+                                        fontSize: 14
+                                    }}>体重  4.1 kg</Text>
+                                    <Text style={{
+                                        marginTop: 5,
+                                        fontSize: 12,
+                                        color: '#8F8F8F'
+                                    }}>4月29日 15.03</Text>
+                                </View>
+                            </View>
+
+                            <View style={{
+                                marginBottom: 15
+                            }}>
+                                <View style={{
+                                    marginLeft: -6,
+                                    height: 10,
+                                    width: 10,
+                                    borderRadius: 10,
+                                    borderStyle: 'solid',
+                                    borderColor: '#F3C026',
+                                    borderWidth: 2,
+                                    backgroundColor: '#F5F5F5'
+                                }}></View>
+                                <View style={{
+                                    marginTop: -15,
+                                    marginLeft: 10,
+                                    marginBottom: 10
+                                }}>
+                                    <Text>3月17日</Text>
+                                </View>
+                                <View style={{
+                                    marginLeft: 10,
+                                    padding: 10,
+                                    backgroundColor: '#fff'
+                                }}>
+                                    <Text style={{
+                                        fontSize: 14
+                                    }}>身高  49 cm</Text>
+                                    <Text style={{
+                                        fontSize: 14
+                                    }}>体重  2.85 kg</Text>
+                                    <Text style={{
+                                        marginTop: 5,
+                                        fontSize: 12,
+                                        color: '#8F8F8F'
+                                    }}>3月17日 16.00</Text>
                                 </View>
                             </View>
                         </View>
