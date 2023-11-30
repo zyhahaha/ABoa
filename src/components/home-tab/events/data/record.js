dbaabcac0a2ca295b686a8fb2cde82c40e99c526
zhaoyang <zhaoyang@lyky.cn>
2023-11-30 16:01:21 +0800

    update

---
+++ src/components/home-tab/events/data/record.js
@@ -0,0 +1,53 @@
+export default [
+    {
+        date: '2023年7月22日',
+        contents: [
+            {
+                url: 'http://1.94.7.83:7788/ABoa/9992.jpg',
+                text: '上传了一张照片',
+                type: 'photo',
+                time: '15:30'
+            }
+        ]
+    }, {
+        date: '2023年6月17日',
+        contents: [
+            {
+                height: '60',
+                weight: '5.6',
+                type: 'BMI',
+                time: '15:00'
+            }
+        ]
+    }, {
+        date: '2023年6月16日',
+        contents: [
+            {
+                url: 'http://1.94.7.83:7788/ABoa/999.jpg',
+                text: '上传了一张照片',
+                type: 'photo',
+                time: '15:00'
+            }
+        ]
+    }, {
+        date: '2023年4月29日',
+        contents: [
+            {
+                height: '54.5',
+                weight: '4.1',
+                type: 'BMI',
+                time: '15:03'
+            }
+        ]
+    }, {
+        date: '2023年3月17日',
+        contents: [
+            {
+                height: '49',
+                weight: '2.85',
+                type: 'BMI',
+                time: '16:00'
+            }
+        ]
+    }
+]
