a53d13d89b9522a73fceca34639f737d4034c744
zhaoyang <zhaoyang@lyky.cn>
2023-12-01 16:11:13 +0800

    update

--- src/home.js
+++ src/home.js
@@ -4,11 +4,17 @@ import React from 'react';
 import Feed from './components/feed'
 import HomeTab from './components/home-tab'
 
-import { SpeedDial } from '@rneui/themed';
+import { SpeedDial, Dialog, Input, CheckBox } from '@rneui/themed';
+import {
+    View
+} from 'react-native';
+import {Stack} from '@rneui/layout'
 
 export default function BottomTabScreen() {
     const [index, setIndex] = React.useState(0);
     const [open, setOpen] = React.useState(false);
+    const [modalVisible, setModalVisible] = React.useState(true);
+    const [checked, setChecked] = React.useState(1);
 
     return (
         <>
@@ -25,35 +31,63 @@ export default function BottomTabScreen() {
                 <SpeedDial.Action
                     icon={{ type: 'entypo', name: 'bowl', color: '#fff' }}
                     title="吃饭"
-                    onPress={() => console.log('Add Something')}
+                    onPress={() => {
+                        setOpen(false)
+                        setModalVisible(true)
+                    }}
                 />
                 <SpeedDial.Action
                     color='#F3C026'
                     icon={{ type: 'font-awesome-5', name: 'poo', color: '#fff' }}
                     title="便便"
-                    onPress={() => console.log('Delete Something')}
+                    onPress={() => {
+                        setOpen(false)
+                    }}
                 />
             </SpeedDial>
-        </>
 
-        // <Button title='Welcome' />
-        // <Icon
-        //     type='feather'
-        //     name='activity'
-        // />
-        // <View>
-        //     <Button
-        //         onPress={() => navigate('LoginScreen')}
-        //         title="Learn More"
-        //         color="#841584"
-        //         accessibilityLabel="Learn more about this purple button"
-        //     />
-        //     <Button
-        //         onPress={() => navigate('UserDetails')}
-        //         title="UserDetails"
-        //         color="#841584"
-        //         accessibilityLabel="UserDetails about this purple button"
-        //     />
-        // </View>
+            <Dialog
+                isVisible={modalVisible}
+                transparent={true}
+                animationType="slide"
+                statusBarTranslucent={true}
+                onBackdropPress={() => setModalVisible(false)}
+            >
+                <Dialog.Title title="记录 12.01" />
+                <Stack row align="center" spacing={1}>
+                    {['今天', '昨天'].map((l, i) => (
+                        <CheckBox
+                        key={i}
+                        title={l}
+                        containerStyle={{ backgroundColor: 'white', borderWidth: 0 }}
+                        checkedIcon="dot-circle-o"
+                        uncheckedIcon="circle-o"
+                        checked={checked === i + 1}
+                        onPress={() => setChecked(i + 1)}
+                        />
+                    ))}
+                </Stack>
+                <View
+                    style={{
+                    }}
+                >
+                    <Input
+                        placeholder=' 时间（几点几分）'
+                        leftIcon={{ type: 'material-community', name: 'clock-digital' }}
+                    />
+                    <Input
+                        placeholder=' 奶量（ml）'
+                        leftIcon={{ type: 'material-community', name: 'baby-bottle-outline' }}
+                    />
+                </View>
+                <Dialog.Button
+                    title="保 存"
+                    onPress={() => {
+                        // console.log(`Option ${checked} was selected!`);
+                        // toggleDialog5();
+                    }}
+                />
+            </Dialog>
+        </>
     );
 }
